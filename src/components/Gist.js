bc8b8479895fcd0389367f5d923e076c70721803
aahadaazar <aahad@sudofy.com>
2023-03-04 18:07:37 +0500

    get all gist feature implemented

--- src/components/Gist.js
+++ src/components/Gist.js
@@ -1,17 +1,116 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { filterGistData } from '../utils/utils';
+import styled from 'styled-components';
 
-function Gist({gist}) {
-  console.log(filterGistData(gist))
-
-  return (
-    <div>Gist</div>
+function Gist({ gist: singleGist }) {
+  return (  
+    <GistWrapper>
+      <GistUserDetail target='_blank' href={singleGist.owner.html_url}>
+        <img className='user-avatar' alt={singleGist.owner.name} src={singleGist.owner.avatar_url} />
+        <p>{singleGist.owner.name}</p>
+      </GistUserDetail>
+      <GistChronoDetails>
+        <p className='date'>{`Created At: ${singleGist.createdAt}`}</p>
+        <p className='date'>{`Last Updated: ${singleGist.updatedAt}`}</p>
+      </GistChronoDetails>
+      <GistDetails>
+        <a target='_blank' href={singleGist.forkUrl} rel="noreferrer"><img className='fork' alt='fork' src='https://user-images.githubusercontent.com/17777237/54873012-40fa5b00-4dd6-11e9-98e0-cc436426c720.png' />Forks</a>
+        <a target='_blank' href={singleGist.comments} rel="noreferrer"><img className='comment' alt='fork' src='https://cdn0.iconfinder.com/data/icons/free-daily-icon-set/512/Comments-512.png' />{singleGist.comments} Comments</a>
+      </GistDetails>
+      <GistDescription>{'Description: ' + (singleGist.description || '-')}</GistDescription>
+      <GistFiles>
+      {
+        singleGist.files.map(file=>{
+          return(
+            <a target='_blank' href={file.url} rel="noreferrer">
+            <img alt='file-icon' className='file-icon' src='https://www.iconpacks.net/icons/2/free-file-icon-1453-thumb.png'/>
+            <p className='file-name'>{file.name}</p>
+            </a>
+          )
+        })
+      }
+      </GistFiles>
+    </GistWrapper>
   )
 }
 
+const GistWrapper = styled.div`
+padding: 12px;
+  display:flex;
+  flex-direction: column;
+  &:not(:last-child) {
+    
+  border-bottom:1px solid #d4d4d4;
+}
+`;
+
+const GistUserDetail = styled.a`
+  display: flex;
+  color: #626465;
+  width: fit-content;
+  margin-bottom:12px;
+  font-size:18px;
+  align-items: center;
+  font-weight:600;
+  .user-avatar{
+    width: 30px;
+height: 30px;
+border-radius: 16px;
+margin-right: 10px;
+  }
+`;
+
+
+const GistChronoDetails = styled.div`
+font-size:12px;
+display: inline-flex;
+margin-bottom:12px;
+.date{
+  margin-right:5px;
+}
+`;
+
+const GistDetails = styled.div`
+font-size:12px;
+display: inline-flex;
+margin-bottom:12px;
+a{
+  font-size:12px;
+cursor:pointer;
+display: inline-flex;
+margin-right:5px;
+img{
+  margin-right:5px;
+margin-top:-2px;
+width:12px;
+}
+.comment{
+  width:16px;
+  margin-top: 0px;
+}
+}
+`;
+
+const GistDescription = styled.p`
+font-size:16px;
+margin-bottom:15px;
+`
+
+const GistFiles = styled.div`
+a{
+  display: inline-flex;
+  margin-right: 10px;
+  
+.file-icon{
+margin-right:5px;
+width: 20px;
+}
+}
+`
+
 Gist.propTypes = {
-  gist:PropTypes.object
+  gist: PropTypes.object
 };
 
 export default Gist
--- src/components/GistList.js
+++ src/components/GistList.js
@@ -52,28 +52,37 @@ const dummyData = {
 }
 
 function GistList({gists}) {
+  console.log(gists)
   return (
     <GistListWrapper>
-      {/* {
-        gists && gists.map(gist=>{
-          return <Gist gist={gist}/>
-        })
-      } */}
       {
-        <Gist gist={dummyData}/>
+        gists === null ? <h3><img className='loading' alt='loading' src='https://upload.wikimedia.org/wikipedia/commons/3/3a/Gray_circles_rotate.gif'/>Fetching Gists</h3>
+        : gists && gists.length === 0 ? <h3>No Gists Found</h3>
+         :gists.map(gist=>{
+          return <Gist key={gist.id} gist={gist}/>
+        })
       }
     </GistListWrapper>
   )
 }
 
 const GistListWrapper = styled.div`
-  margin-top:20px;
   min-width:70vw;
   max-height:calc(100vh - 120px);
   overflow: auto;
   box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
   align-self:center;
   padding: 8px 0px;
+  margin:5px 30px;
+  h3{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .loading{
+      width: 20px;
+      margin-right: 10px;
+    }
+  }
 `;
 
 GistList.propTypes = {
